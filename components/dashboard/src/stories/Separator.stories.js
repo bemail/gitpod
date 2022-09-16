7026bf7ebfcd552b21075ec3d89797ac499625e7
Laurie T. Malau <laurie@gitpod.io>
2022-09-16 13:45:13 +0000

    [dashboard] Storybook trial

---
+++ components/dashboard/src/stories/Separator.stories.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
+import Separator from "../components/Separator";
+
+export default {
+    title: "Separator",
+    component: Separator,
+};
+
+export const Template = () => <Separator />;
